db8fb0c50d10de54d9de9a3db518b4a8b7d74152
Marton Jani <martonjanos1990@gmail.com>
2024-12-22 15:30:47 +0100

    Bugfix. If POST part is deleted due to causing CORS error

--- api/task/deleteLocker.php
+++ api/task/deleteLocker.php
@@ -7,44 +7,43 @@ require('../../api/user/auth/auth.php');
 
 $response = [];
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $jsonData = file_get_contents("php://input");
-    $newItems = json_decode($jsonData, true);
+$jsonData = file_get_contents("php://input");
+$newItems = json_decode($jsonData, true);
 
-    class RemoveLocker
+class RemoveLocker
+{
+    private $conn;
+    private $response;
+    private $auth;
+
+    public function __construct($conn, &$response, $auth)
     {
-        private $conn;
-        private $response;
-        private $auth;
+        $this->conn = $conn;
+        $this->response = &$response;
+        $this->auth = $auth;
+    }
 
-        public function __construct($conn, &$response, $auth)
-        {
-            $this->conn = $conn;
-            $this->response = &$response;
-            $this->auth = $auth;
+    //User validation here
+    public function Auth()
+    {
+        return $this->auth->authenticate();
+    }
+
+    public function removeLockerFunction($conn, $newItems)
+    {
+        $userId = null;
+        $isAccess = $this->auth->authenticate(10);
+        if ($isAccess['status'] !== 200) {
+            return $this->response = $isAccess;
+        } else {
+            $userId = $isAccess['data']->userId;
         }
 
-        //User validation here
-        public function Auth()
-        {
-            return $this->auth->authenticate();
-        }
-
-        public function removeLockerFunction($conn, $newItems)
-        {
-            $userId = null;
-            $isAccess = $this->auth->authenticate(10);
-            if ($isAccess['status'] !== 200) {
-                return $this->response = $isAccess;
-            } else {
-                $userId = $isAccess['data']->userId;
-            }
-
-            $result = removeLocker($conn, $newItems, $userId);
-            $this->response = $result;
-        }
+        $result = removeLocker($conn, $newItems, $userId);
+        $this->response = $result;
     }
 }
+
 $tokenRow = $_SERVER['HTTP_AUTHORIZATION'];
 preg_match('/Bearer\s(\S+)/', $tokenRow, $matches);
 $token = $matches[1];
