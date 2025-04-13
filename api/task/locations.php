317e7e49da262e08cf2b8fb8cfa4dad5bfad4e64
Jani <martonjanos1990@gmail.com>
2025-04-13 11:05:41 +0200

    .

--- api/task/locations.php
+++ api/task/locations.php
@@ -1,202 +1,11 @@
 <?php
+
 $locations = [
     [
-        "LockerStationID" => "11280",
-        "BranchName" => "Pajor Andrea - Kassai u - ARKA S",
-        "City" => "Székesfehérvár",
-        "Address" => "Kassai u 43",
-        "Latitude" => "47.525803789831734",
-        "Longitude" => "19.05733392401584",
-        "Timetable" => [
-            [
-                "workingDay" => "Monday",
-                "startTime" => "00:00",
-                "endTime" => "23:59"
-            ],
-            [
-                "workingDay" => "Tuesday",
-                "startTime" => "00:00",
-                "endTime" => "23:59"
-            ],
-            [
-                "workingDay" => "Wednesday",
-                "startTime" => "00:00",
-                "endTime" => "23:59"
-            ],
-            [
-                "workingDay" => "Thursday",
-                "startTime" => "00:00",
-                "endTime" => "23:59"
-            ],
-            [
-                "workingDay" => "Friday",
-                "startTime" => "00:00",
-                "endTime" => "23:59"
-            ],
-            [
-                "workingDay" => "Saturday",
-                "startTime" => "00:00",
-                "endTime" => "23:59"
-            ],
-            [
-                "workingDay" => "Sunday",
-                "startTime" => "00:00",
-                "endTime" => "23:59"
-            ]
-        ],
-        "DropOffCapability" => true,
-        "CountryCode" => "HU",
-        "Zip" => "8000"
-    ],
-    [
-        "LockerStationID" => "11281",
-        "BranchName" => "D-M-M-D Kft. - Kossuth L - ARKA S",
-        "City" => "Nyergesújfalu",
-        "Address" => "Kossuth L. u. 252.",
-        "Latitude" => "47.525803789831734",
-        "Longitude" => "19.05733392401584",
-        "Timetable" => [
-            [
-                "workingDay" => "Monday",
-                "startTime" => "00:00",
-                "endTime" => "23:59"
-            ],
-            [
-                "workingDay" => "Tuesday",
-                "startTime" => "00:00",
-                "endTime" => "23:59"
-            ],
-            [
-                "workingDay" => "Wednesday",
-                "startTime" => "00:00",
-                "endTime" => "23:59"
-            ],
-            [
-                "workingDay" => "Thursday",
-                "startTime" => "00:00",
-                "endTime" => "23:59"
-            ],
-            [
-                "workingDay" => "Friday",
-                "startTime" => "00:00",
-                "endTime" => "23:59"
-            ],
-            [
-                "workingDay" => "Saturday",
-                "startTime" => "00:00",
-                "endTime" => "23:59"
-            ],
-            [
-                "workingDay" => "Sunday",
-                "startTime" => "00:00",
-                "endTime" => "23:59"
-            ]
-        ],
-        "DropOffCapability" => true,
-        "CountryCode" => "HU",
-        "Zip" => "2536"
-    ],
-    [
-        "LockerStationID" => "11282",
-        "BranchName" => "MOL - Mezőkövesd út - ARKA S",
-        "City" => "Budapest XI.",
-        "Address" => "Mezőkövesd út 11.",
-        "Latitude" => "47.525803789831734",
-        "Longitude" => "19.05733392401584",
-        "Timetable" => [
-            [
-                "workingDay" => "Monday",
-                "startTime" => "00:00",
-                "endTime" => "23:59"
-            ],
-            [
-                "workingDay" => "Tuesday",
-                "startTime" => "00:00",
-                "endTime" => "23:59"
-            ],
-            [
-                "workingDay" => "Wednesday",
-                "startTime" => "00:00",
-                "endTime" => "23:59"
-            ],
-            [
-                "workingDay" => "Thursday",
-                "startTime" => "00:00",
-                "endTime" => "23:59"
-            ],
-            [
-                "workingDay" => "Friday",
-                "startTime" => "00:00",
-                "endTime" => "23:59"
-            ],
-            [
-                "workingDay" => "Saturday",
-                "startTime" => "00:00",
-                "endTime" => "23:59"
-            ],
-            [
-                "workingDay" => "Sunday",
-                "startTime" => "00:00",
-                "endTime" => "23:59"
-            ]
-        ],
-        "DropOffCapability" => true,
-        "CountryCode" => "HU",
-        "Zip" => "1116"
-    ],
-    [
-        "LockerStationID" => "11283",
-        "BranchName" => "Vörösvár kft. - Hűvösvölgyi u - ARKA S",
-        "City" => "Budapest II.",
-        "Address" => "1021. Bp., II. Hűvösvölgyi u. 135.",
-        "Latitude" => "47.525803789831734",
-        "Longitude" => "19.05733392401584",
-        "Timetable" => [
-            [
-                "workingDay" => "Monday",
-                "startTime" => "00:00",
-                "endTime" => "23:59"
-            ],
-            [
-                "workingDay" => "Tuesday",
-                "startTime" => "00:00",
-                "endTime" => "23:59"
-            ],
-            [
-                "workingDay" => "Wednesday",
-                "startTime" => "00:00",
-                "endTime" => "23:59"
-            ],
-            [
-                "workingDay" => "Thursday",
-                "startTime" => "00:00",
-                "endTime" => "23:59"
-            ],
-            [
-                "workingDay" => "Friday",
-                "startTime" => "00:00",
-                "endTime" => "23:59"
-            ],
-            [
-                "workingDay" => "Saturday",
-                "startTime" => "00:00",
-                "endTime" => "23:59"
-            ],
-            [
-                "workingDay" => "Sunday",
-                "startTime" => "00:00",
-                "endTime" => "23:59"
-            ]
-        ],
-        "DropOffCapability" => true,
-        "CountryCode" => "HU",
-        "Zip" => "1021"
-    ],
-    [
-        "LockerStationID" => "11284",
-        "BranchName" => "Vörösvár kft. - Páskomliget u - ARKA S",
+        "LockerStationID" => "11352",
+        "BranchName" => "Budapest(III) - Zay utca - ARKA S",
         "City" => "Budapest",
-        "Address" => "1156. Bp., XV. Páskomliget u. 49.",
+        "Address" => "Zay utca 5",
         "Latitude" => "47.525803789831734",
         "Longitude" => "19.05733392401584",
         "Timetable" => [
@@ -238,15 +47,15 @@ $locations = [
         ],
         "DropOffCapability" => true,
         "CountryCode" => "HU",
-        "Zip" => "1156"
+        "Zip" => "1037"
     ],
     [
-        "LockerStationID" => "11285",
-        "BranchName" => "Kaposvári Nagypiac Kft. - Baross Gábor utca - ARKA S",
-        "City" => "Kaposvár",
-        "Address" => "Baross Gábor utca 5-13",
+        "LockerStationID" => "11353",
+        "BranchName" => "Szolnok - Pozsonyi út - ARKA S",
+        "City" => "Szolnok",
+        "Address" => "Pozsonyi út",
         "Latitude" => "47.525803789831734",
-        "Longitude" => "19.05733392401584",
+        "Longitude" => "20.181",
         "Timetable" => [
             [
                 "workingDay" => "Monday",
@@ -286,15 +95,15 @@ $locations = [
         ],
         "DropOffCapability" => true,
         "CountryCode" => "HU",
-        "Zip" => "7400"
+        "Zip" => "5000"
     ],
     [
-        "LockerStationID" => "11286",
-        "BranchName" => "Tánczos-Szabó Zoltán - Boróka utca - ARKA S",
-        "City" => "Kecskemét",
-        "Address" => "Boróka utca 43",
+        "LockerStationID" => "11354",
+        "BranchName" => "Fót - Szabó Dezső utca - ARKA S",
+        "City" => "Fót",
+        "Address" => "Szabó Dezső utca 2/B.",
         "Latitude" => "47.525803789831734",
-        "Longitude" => "19.05733392401584",
+        "Longitude" => "19.236",
         "Timetable" => [
             [
                 "workingDay" => "Monday",
@@ -334,15 +143,15 @@ $locations = [
         ],
         "DropOffCapability" => true,
         "CountryCode" => "HU",
-        "Zip" => "6000"
+        "Zip" => "2151"
     ],
     [
-        "LockerStationID" => "11287",
-        "BranchName" => "COOP Hungary Zrt. - Hargita u - ARKA S",
-        "City" => "Szeged",
-        "Address" => "Hargita u.60.",
+        "LockerStationID" => "11355",
+        "BranchName" => "Budapest(VIII) - Teleki tér - ARKA S",
+        "City" => "Budapest",
+        "Address" => "Teleki tér",
         "Latitude" => "47.525803789831734",
-        "Longitude" => "19.05733392401584",
+        "Longitude" => "19.084",
         "Timetable" => [
             [
                 "workingDay" => "Monday",
@@ -382,15 +191,15 @@ $locations = [
         ],
         "DropOffCapability" => true,
         "CountryCode" => "HU",
-        "Zip" => "6726"
+        "Zip" => "1086"
     ],
     [
-        "LockerStationID" => "11288",
-        "BranchName" => "Molnár Centerker Kft - Liget Autómosó - ARKA S",
-        "City" => "Gyömrő",
-        "Address" => "Kóczán út",
+        "LockerStationID" => "11356",
+        "BranchName" => "Kóka - Dózsa György út - ARKA S",
+        "City" => "Kóka",
+        "Address" => "Dózsa György út 32",
         "Latitude" => "47.525803789831734",
-        "Longitude" => "19.05733392401584",
+        "Longitude" => "19.366",
         "Timetable" => [
             [
                 "workingDay" => "Monday",
@@ -430,15 +239,15 @@ $locations = [
         ],
         "DropOffCapability" => true,
         "CountryCode" => "HU",
-        "Zip" => "2230"
+        "Zip" => "2243"
     ],
     [
-        "LockerStationID" => "11289",
-        "BranchName" => "Orosz Máté - Kossuth utca - ARKA S",
-        "City" => "Alsózsolca",
-        "Address" => "Kossuth utca 49",
+        "LockerStationID" => "11357",
+        "BranchName" => "Budapest(XVI) - Rákosi út - ARKA S",
+        "City" => "Budapest",
+        "Address" => "Rákosi út 149.",
         "Latitude" => "47.525803789831734",
-        "Longitude" => "19.05733392401584",
+        "Longitude" => "19.084",
         "Timetable" => [
             [
                 "workingDay" => "Monday",
@@ -478,6 +287,7 @@ $locations = [
         ],
         "DropOffCapability" => true,
         "CountryCode" => "HU",
-        "Zip" => "3571"
+        "Zip" => "1161"
     ]
+
 ];
