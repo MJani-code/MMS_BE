e488c428cc1f7fab6a0c531fabaedd32fcdeea09
Jani <martonjanos1990@gmail.com>
2025-09-14 18:57:37 +0200

    change auth.php so only authorized roleId can see the requested path

--- api/auth.php
+++ api/auth.php
@@ -32,12 +32,14 @@ class AuthHandler
             'data' => $data,
         ]);
     }
+
     public function handleAuth()
     {
         $jsonData = file_get_contents("php://input");
         $data = json_decode($jsonData, true);
 
         $token = $data['token'] ?? NULL;
+        $urlTo = $data['urlTo'] ?? NULL;
 
         //Jött Token?
         if (!$token) {
@@ -48,20 +50,40 @@ class AuthHandler
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             try {
                 $decoded = JWT::decode($token, new Key($this->secretKey, 'HS256'));
+                // Ellenőrizzük a token lejárati idejét
                 if ($decoded->expirationTime < time()) {
                     // A token lejárt
                     echo $this->createResponse(401, 'A token lejárt, kérjük jelentkezz be újra.');
                     return;
                 } else {
                     // A token még érvényes. Leellenőrizzük, hogy a token létezik-e az adatbázisban
-                    $query = "SELECT * FROM user_login WHERE token = :token";
+                    $query = "SELECT *
+                    FROM user_login                    
+                    WHERE token = :token";
                     $stmt = $this->conn->prepare($query);
                     $stmt->execute(['token' => $token]);
                     $user = $stmt->fetch(PDO::FETCH_ASSOC);
                     if (!$user) {
                         echo $this->createResponse(400, 'Hiányzik a token.');
-                    }else{
-                        echo $this->createResponse(200, 'Érvényes token.');
+                    } else {
+                        //urlTo kikérdezése adatbázisból
+                        $userId = $user['user_id'];
+                        $query = "SELECT rr.is_visible, rr.role_id
+                          FROM role_routes rr
+                          LEFT JOIN users u ON rr.role_id = u.role_id
+                          JOIN routes r ON rr.route_id = r.id
+                          WHERE r.path = :urlTo AND u.id = :userId";
+                        $stmt = $this->conn->prepare($query);
+                        $stmt->execute(['urlTo' => $urlTo, 'userId' => $userId]);
+                        $isPathAccessible = $stmt->fetchColumn();
+
+                        if (!$isPathAccessible) {
+                            http_response_code(404);
+                            echo $this->createResponse(404, 'Hozzáférés megtagadva', ['urlTo' => '/admin/tasks', 'title' => 'Megbízások']);
+                            return;
+                        }
+
+                        echo $this->createResponse(200, 'Érvényes token.', $user);
                     }
                 }
             } catch (Exception $e) {
