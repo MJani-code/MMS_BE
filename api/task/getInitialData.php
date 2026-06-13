057dc8ecb277b61c6b5c49ecc5a64a5e57de2efd
Jani <martonjanos1990@gmail.com>
2026-06-13 17:24:34 +0200

    update/multilanguage support is added to getInitialData file

--- api/task/getInitialData.php
+++ api/task/getInitialData.php
@@ -66,7 +66,7 @@ class GetInitialData
                 "SELECT DISTINCT t.text, tc.dbTable, tc.dbColumn, tc.align, tc.filterable, tc.value
                  FROM task_columns tc
                  LEFT JOIN task_column_permissions tcp ON tcp.task_columns_id = tc.id
-                 LEFT JOIN translations t ON t.task_columns_id = tc.id AND t.locale = :locale
+                 LEFT JOIN translations t ON t.task_column_id = tc.id AND t.locale = :locale
                  WHERE tc.task_column_types_id = 1
                    AND tc.is_active = 1
                    AND (tcp.role_id IS NULL OR tcp.role_id >= :role_id)
@@ -75,30 +75,37 @@ class GetInitialData
             );
 
             $statuses = $this->fetchAll(
-                "SELECT ts.id, ts.name, ts.color
+                "SELECT ts.id, t.text AS name, ts.color
                  FROM task_statuses ts
-                 WHERE ts.is_active = 1"
+                 LEFT JOIN translations t on t.task_status_id = ts.id AND t.locale = :locale
+                 WHERE ts.is_active = 1",
+                ['locale' => $this->locale]
             );
 
             $allowedStatuses = $this->fetchAll(
-                "SELECT DISTINCT ts.id, ts.name, ts.color
+                "SELECT DISTINCT ts.id, t.text AS name, ts.color
                  FROM task_statuses ts
                  LEFT JOIN task_status_permissions tsp ON tsp.task_status_id = ts.id
+                 LEFT JOIN translations t on t.task_status_id = ts.id AND t.locale = :locale
                  WHERE ts.is_active = 1
                    AND (tsp.role_id IS NULL OR tsp.role_id >= :role_id)",
-                $params
+                array_merge($params, ['locale' => $this->locale])
             );
 
             $locationTypes = $this->fetchAll(
-                "SELECT lt.id, lt.name, lt.color
+                "SELECT lt.id, t.text AS name, lt.color
                  FROM location_types lt
-                 WHERE lt.is_active = 1"
+                 LEFT JOIN translations t ON t.location_type_id = lt.id AND t.locale = :locale
+                 WHERE lt.is_active = 1",
+                ['locale' => $this->locale]
             );
 
             $taskTypes = $this->fetchAll(
-                "SELECT ttd.id, ttd.name, ttd.color
+                "SELECT ttd.id, t.text AS name, ttd.color
                  FROM task_type_details ttd
-                 WHERE ttd.is_active = 1"
+                 LEFT JOIN translations t ON t.task_type_detail_id = ttd.id AND t.locale = :locale
+                 WHERE ttd.is_active = 1",
+                ['locale' => $this->locale]
             );
 
             $responsibles = $this->fetchAll(
@@ -109,16 +116,20 @@ class GetInitialData
             );
 
             $priorities = $this->fetchAll(
-                "SELECT p.id, p.name, p.color
+                "SELECT p.id, t.text AS name, p.color
                  FROM priorities p
-                 WHERE p.is_active = 1"
+                 LEFT JOIN translations t ON t.priority_id = p.id AND t.locale = :locale
+                 WHERE p.is_active = 1",
+                ['locale' => $this->locale]
             );
 
             $statusesGroupsResult = $this->fetchAll(
-                "SELECT t.status_by_exohu_id AS id, ts.name, ts.color, COUNT(t.id) AS count
+                "SELECT t.status_by_exohu_id AS id, tr.text AS name, ts.color, COUNT(t.id) AS count
                  FROM tasks t
                  LEFT JOIN task_statuses ts ON ts.id = t.status_by_exohu_id
-                 GROUP BY t.status_by_exohu_id, ts.name, ts.color"
+                 LEFT JOIN translations tr ON tr.task_status_id = ts.id AND tr.locale = :locale
+                 GROUP BY t.status_by_exohu_id, tr.text, ts.color",
+                ['locale' => $this->locale]
             );
 
             $statusGroups = [];
